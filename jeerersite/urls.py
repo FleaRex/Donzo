478e298446b8e31c0f78116b5d2fd652b9703112
Ben Madley <bmad29@hotmail.co.uk>
2020-01-12 15:12:35 +0000

    Move Donzo to /donzo

--- jeerersite/urls.py
+++ jeerersite/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('jeerer/', include('jeerer_app.urls')),
+    path('donzo/', include('jeerer_app.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
